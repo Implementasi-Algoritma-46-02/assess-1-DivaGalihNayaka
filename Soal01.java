f7e0d1de66b1be43e2d5f4fbd743b0c89f257e6b
Diva Galih Nayaka <divagalihn@gmail.com>
2024-10-29 17:30:07 +0700

    assesmen

--- Soal01.java
+++ Soal01.java
@@ -1,6 +1,22 @@
+import java.util.Scanner;
 public class Soal01 {
 
 	public static void main(final String[] args) {
 		// Kerjakan soalnya di sini
-	}
+		Scanner scanner = new Scanner(System.in);
+
+		int digitPertama = 123;
+        int digitKedua = 456;
+		int digitKetiga = 789;
+		int digitKeempat = 101;
+		int digitKelima = 135;
+
+		int hasil = (digitPertama, digitKedua, digitKetiga, digitKeempat, digitKelima);
+		System.out.println(hasil );
+
+
+
+		
+        scanner.close();
+    }
 }
--- Soal04.java
+++ Soal04.java
@@ -1,6 +1,17 @@
+import java.util.Scanner;
 public class Soal04 {
 
-	public static void main(final String[] args) {
+	public static void main(String[] args) {
 		// Kerjakan soalnya di sini
+		Scanner scanner = new Scanner(System.in);
+
+		String[] kode dan jam = scanner.nextLine().split(" ");
+
+		String kode = kode dan jam[0];
+
+		int jamsebulan = Integer.parseInt(jam[1]);
+	}
+	public static String(String[] args) {
+		
 	}
 }
