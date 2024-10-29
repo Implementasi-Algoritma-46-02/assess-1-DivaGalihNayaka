5b5b4696f9f7edae3f046908db6316285f5eb96e
Diva Galih Nayaka <divagalihn@gmail.com>
2024-10-29 17:51:53 +0700

    assesmen

--- Soal01.java
+++ Soal01.java
@@ -3,28 +3,32 @@ public class Soal01 {
 
 	public static void main(final String[] args) {
 		// Kerjakan soalnya di sini
-		Scanner scanner = new Scanner(System.in);
-
-        try {
-            
-            System.out.print("");
-            if (scanner.hasNextInt()) {  
-                int nomor = scanner.nextInt();
-
-				System.out.println("");
-                System.out.println("123 " + (123 > nomor ? "valid" : "tidak valid"));
-                System.out.println("456 " + (456 < nomor ? "valid" : "tidak valid"));
-                System.out.println("789 " + (789 > nomor ? "valid" : "tidak valid"));
-                System.out.println("101 " + (101 > nomor ? "valid" : "tidak valid"));
-                System.out.println("135 " + (135 > nomor ? "valid" : "tidak valid"));
-            } else {
-                System.out.println("Error: Input harus berupa angka.");
-            }
-        } catch (Exception e) {
-            System.out.println("Terjadi kesalahan: " + e.getMessage());
-        } finally {
-;
-            scanner.close();
-        }
+		Scanner userInput = new Scanner(System.in);
+        int bil1 = userInput.nextInt();
+        int bill2 = userInput.nextInt();
+        int bill3 = userInput.nextInt();
+        int bill4 = userInput.nextInt();
+        int bill5 = userInput.nextInt();
+        System.out.println(identifikasi(bil1));
+        System.out.println(identifikasi(bill2));
+        System.out.println(identifikasi(bill3));
+        System.out.println(identifikasi(bill4));
+        System.out.println(identifikasi(bill5));
     }
+
+    public static String identifikasi(int bilangan) {
+        int k1 = bilangan / 100;
+        int k2 = bilangan / 10 % 10;
+        int k3 = bilangan % 10;
+        String valid = "";
+        boolean cekSama = (k1 != k2) && (k2 != k3) && (k3 != k1);
+        boolean cekBesar = (k1 < k2) && (k2 < k3);
+        boolean cekGanjil = (k3 % 2) == 1;
+        if (cekSama && cekBesar && cekGanjil) {
+            valid = "valid";
+        } else {
+            valid = "tidak valid";
+        }
+        return String.format("%d %s", bilangan, valid);
+	}
 }
